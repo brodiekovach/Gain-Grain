a67be2a3c518818465dafa0bc570e306722f0eba
teisenba <teisenba@vols.utk.edu>
2024-11-12 21:39:57 -0500

    small stuff for comments

--- gain-grain/src/utils/postModels/Post.js
+++ gain-grain/src/utils/postModels/Post.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const commentSchema = new mongoose.Schema({
     comment: { type: String, required: true },
-    date: { type: Date, required: true },
+    date: { type: Date, default: Date.now },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
